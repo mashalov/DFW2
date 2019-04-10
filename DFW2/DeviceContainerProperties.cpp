cf791180e3fc898be2823948961bc791d8100568
Eugene Mashalov <mashalov@gmail.com>
2019-04-10 23:04:16 +0500

    comments кэширование имени переменной в CDevice::LinkToContainer

--- DFW2/DeviceContainerProperties.cpp
+++ DFW2/DeviceContainerProperties.cpp
@@ -22,7 +22,13 @@ void CDeviceContainerProperties::SetType(eDFW2DEVICETYPE eDevType)
 	m_TypeInfoSet.insert(eDeviceType = eDevType);
 }
 
+// добавить возможность связи от устройства
+// указываем тип устройства, режим связи и тип зависимости. Режим связи и тип зависимости указываются для внешнего устройства
+// вызов этой функции для контейнера означает, что контейнер должен принимать связи от заданного типа устройств, считая заднный тип устройства ведущим или подчиненным
+// данная функция кроме всего предыдущего принимает еще имя константы во внешнем устройстве, в которой задан номер из этого контейнера
 
+// связи контейнеров двух типов a и b должны быть взаимными - a.From b, b.To a, в зависимости от того, в каком контейнере есть константа идентификатора для связи
+// eDFW2DEVICEDEPENDENCY должны быть комплиментарны - Master - Slave.
 void CDeviceContainerProperties::AddLinkTo(eDFW2DEVICETYPE eDevType, eDFW2DEVICELINKMODE eLinkMode, eDFW2DEVICEDEPENDENCY Dependency, const _TCHAR* cszstrIdField)
 {
 	if (m_LinksTo.find(eDevType) == m_LinksTo.end())
@@ -34,6 +40,7 @@ void CDeviceContainerProperties::AddLinkTo(eDFW2DEVICETYPE eDevType, eDFW2DEVICE
 }
 
 // добавить возможность связи от устройства
+// указываем тип устройства, режим связи и тип зависимости. Режим связи и тип зависимости указываются для внешнего устройства
 void CDeviceContainerProperties::AddLinkFrom(eDFW2DEVICETYPE eDevType, eDFW2DEVICELINKMODE eLinkMode, eDFW2DEVICEDEPENDENCY Dependency)
 {
 	if (m_LinksFrom.find(eDeviceType) == m_LinksFrom.end())
