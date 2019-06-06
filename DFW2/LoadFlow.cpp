557260e62eddb1dcfa5f3f521b0e3fe64b467734
Eugene Mashalov <mashalov@gmail.com>
2019-06-07 04:14:16 +0500

    LF PV

--- DFW2/LoadFlow.cpp
+++ DFW2/LoadFlow.cpp
@@ -48,11 +48,6 @@ bool CDynaModel::LoadFlow()
 	return LoadFlow.Run();
 }
 
-bool CLoadFlow::SetElement(ptrdiff_t nRow, ptrdiff_t nCol, double& dValue, bool bAdd)
-{
-	return true;
-}
-
 bool CLoadFlow::Estimate()
 {
 	bool bRes = false;
@@ -71,6 +66,8 @@ bool CLoadFlow::Estimate()
 	{
 		CDynaNodeBase *pNode = static_cast<CDynaNodeBase*>(*it);
 		pNode->Init(m_pDynaModel);
+		//pNode->V0 = pNode->Unom;
+
 
 		if (NodeInMatrix(pNode))
 		{
@@ -167,12 +164,9 @@ bool CLoadFlow::Estimate()
 	return bRes = true;
 }
 
-bool CLoadFlow::Run()
+bool CLoadFlow::BuildMatrix()
 {
-	bool bRes = Estimate();
-	if (!bRes)
-		return bRes;
-
+	bool bRes = true;
 	double *pb = b;
 	double *pAx = Ax;
 	_MatrixInfo *pMatrixInfo = m_pMatrixInfo;
@@ -183,12 +177,9 @@ bool CLoadFlow::Run()
 		if (NodeInMatrix(pNode))
 		{
 			pNode->GetPnrQnr();
-			double Pe = pNode->GetSelfImbP();
-			double Qe = pNode->GetSelfImbQ();
-			double dPdDelta = 0.0;
-			double dPdV = pNode->GetSelfdPdV();
-			double dQdDelta = 0.0;
-			double dQdV = pNode->GetSelfdQdV();
+			double Pe = pNode->GetSelfImbP(), Qe = pNode->GetSelfImbQ();
+			double dPdDelta = 0.0, dQdDelta = 0.0;
+			double dPdV = pNode->GetSelfdPdV(), dQdV = pNode->GetSelfdQdV();
 
 			double *pAxSelf = pAx;
 			pAx += 2;
@@ -196,6 +187,9 @@ bool CLoadFlow::Run()
 			CLinkPtrCount *pBranchLink = pNode->GetLink(0);
 			pNode->ResetVisited();
 			CDevice **ppDevice = nullptr;
+
+			bool bNodePQ = pNode->IsLFTypePQ();
+
 			while (pBranchLink->In(ppDevice))
 			{
 				CDynaBranch *pBranch = static_cast<CDynaBranch*>(*ppDevice);
@@ -203,46 +197,58 @@ bool CLoadFlow::Run()
 				{
 					// определяем узел на противоположном конце инцидентной ветви
 					CDynaNodeBase *pOppNode = pBranch->GetOppositeNode(pNode);
-					cplx mult = conj(pNode->VreVim);
-					// определяем взаимную проводимость со смежным узлом
-					cplx *pYkm = pBranch->m_pNodeIp == pNode ? &pBranch->Yip : &pBranch->Yiq;
-					mult *= pOppNode->VreVim ** pYkm;
-					Pe -= mult.real();
-					Qe += mult.imag();
-
-					// diagonals 2
-					dPdDelta -= mult.imag();
-					dPdV += -CDevice::ZeroDivGuard(mult.real(), pNode->V);
-					dQdDelta += -mult.real();
-					dQdV += CDevice::ZeroDivGuard(mult.imag(), pNode->V);
-
-					if (NodeInMatrix(pOppNode))
+					if (pOppNode->IsStateOn())
 					{
-						ptrdiff_t DupIndex = pNode->CheckAddVisited(pOppNode);
-						if (DupIndex < 0)
+						cplx mult = conj(pNode->VreVim);
+						// определяем взаимную проводимость со смежным узлом
+						cplx *pYkm = pBranch->m_pNodeIp == pNode ? &pBranch->Yip : &pBranch->Yiq;
+						mult *= pOppNode->VreVim ** pYkm;
+						Pe -= mult.real();
+						Qe += mult.imag();
+
+						dPdDelta -= mult.imag();
+						dPdV += -CDevice::ZeroDivGuard(mult.real(), pNode->V);
+
+						dQdDelta += -mult.real();
+						dQdV += CDevice::ZeroDivGuard(mult.imag(), pNode->V);
+
+						if (NodeInMatrix(pOppNode))
 						{
-							//_ASSERTE(pAx - pAxSelf < pMatrixInfo->nRowCount);
-							*pAx = mult.imag();
-							*(pAx + pMatrixInfo->nRowCount) = mult.real();
-							pAx++;
-							*pAx = -CDevice::ZeroDivGuard(mult.real(), pOppNode->V);
-							*(pAx + pMatrixInfo->nRowCount) = CDevice::ZeroDivGuard(mult.imag(), pOppNode->V);
-							pAx++;
-						}
-						else
-						{
-							double *pSumAx = pAxSelf + 2 + DupIndex;
-							*pSumAx += mult.imag();
-							*(pSumAx + pMatrixInfo->nRowCount) += mult.real();
-							pSumAx++;
-							*pSumAx += -CDevice::ZeroDivGuard(mult.real(), pOppNode->V);
-							*(pSumAx + pMatrixInfo->nRowCount) += CDevice::ZeroDivGuard(mult.imag(), pOppNode->V);
+							double dPdDeltaM = mult.imag();
+							double dPdVM = -CDevice::ZeroDivGuard(mult.real(), pOppNode->V);
+							double dQdDeltaM = mult.real();
+							double dQdVM = CDevice::ZeroDivGuard(mult.imag(), pOppNode->V);
+
+							if (!bNodePQ)
+							{
+								dQdDeltaM = dQdVM = 0.0;
+							}
+
+							ptrdiff_t DupIndex = pNode->CheckAddVisited(pOppNode);
+							if (DupIndex < 0)
+							{
+								*pAx = dPdDeltaM;
+								*(pAx + pMatrixInfo->nRowCount) = dQdDeltaM;
+								pAx++;
+								*pAx = dPdVM;
+								*(pAx + pMatrixInfo->nRowCount) = dQdVM;
+								pAx++;
+							}
+							else
+							{
+								double *pSumAx = pAxSelf + 2 + DupIndex;
+								*pSumAx += dPdDeltaM;
+								*(pSumAx + pMatrixInfo->nRowCount) += dQdDeltaM;
+								pSumAx++;
+								*pSumAx += dPdVM;
+								*(pSumAx + pMatrixInfo->nRowCount) += dQdVM;
+							}
 						}
 					}
 				}
-			
+
 				/*
-				
+
 				{
 					// dP/dDelta
 					pDynaModel->SetElement(A(0), pOppNode->A(0), mult.imag(), bDup);
@@ -256,12 +262,19 @@ bool CLoadFlow::Run()
 				*/
 			}
 
+			if (!bNodePQ)
+			{
+				dQdDelta = 0.0;
+				dQdV = 1.0;
+				Qe = 0.0;
+			}
+
 			*pAxSelf = dPdDelta;
 			*(pAxSelf + pMatrixInfo->nRowCount) = dQdDelta;
 			pAxSelf++;
 			*pAxSelf = dPdV;
 			*(pAxSelf + pMatrixInfo->nRowCount) = dQdV;
-
+		
 			pAx += pMatrixInfo->nRowCount;
 
 			/*
@@ -277,9 +290,107 @@ bool CLoadFlow::Run()
 
 		pMatrixInfo++;
 	}
+	return bRes;
+}
 
-	KLU_numeric *Numeric = KLU_factor(Ai, Ap, Ax, Symbolic, &Common);
-	int sq = KLU_tsolve(Symbolic, Numeric, m_nMatrixSize, 1, b, &Common);
-	KLU_free_numeric(&Numeric, &Common);
+#include "atlbase.h"
+bool CLoadFlow::Run()
+{
+	/*
+	CDeviceContainer *pBranchCon = m_pDynaModel->GetDeviceContainer(DEVTYPE_BRANCH);
+	CDynaBranch *pBr = static_cast<CDynaBranch*>(*(pBranchCon->begin() + 4));
+	pBr->SetBranchState(CDynaBranch::BRANCH_OFF, eDEVICESTATECAUSE::DSC_INTERNAL);
+	*/
+
+	bool bRes = Estimate();
+	if (!bRes)
+		return bRes;
+
+	CDynaNodeBase *pNo = static_cast<CDynaNodeBase*>(*(pNodes->begin()));
+	pNo->Pn += 2;
+
+
+	for (int it = 0; it < 10; it++)
+	{
+		BuildMatrix();
+		KLU_numeric *Numeric = KLU_factor(Ai, Ap, Ax, Symbolic, &Common);
+
+		int nmx = 0;
+		for (int i = 1; i < m_nMatrixSize; i++)
+		{
+			if (fabs(b[nmx]) < fabs(b[i]))
+				nmx = i;
+		}
+		ATLTRACE("\n%g", b[nmx]);
+
+
+		int sq = KLU_tsolve(Symbolic, Numeric, m_nMatrixSize, 1, b, &Common);
+		/*
+		nmx = 0;
+		for (int i = 1; i < m_nMatrixSize; i++)
+		{
+			if (fabs(b[nmx]) < fabs(b[i]))
+				nmx = i;
+		}
+		ATLTRACE("\n%g", b[nmx]);
+		*/
+	
+		KLU_free_numeric(&Numeric, &Common);
+
+		
+
+		_MatrixInfo *pMatrixInfo = m_pMatrixInfo;
+		for (DEVICEVECTORITR it = pNodes->begin(); it != pNodes->end(); it++)
+		{
+			CDynaNodeBase *pNode = static_cast<CDynaNodeBase*>(*it);
+			if (NodeInMatrix(pNode))
+			{
+				pNode->Delta -= b[pNode->A(0)];
+				pNode->V -= b[pNode->A(0) + 1];
+				double dV = b[pNode->A(0) + 1];
+				pNode->UpdateVreVim();
+			}
+		}
+
+
+		for (DEVICEVECTORITR it = pNodes->begin(); it != pNodes->end(); it++)
+		{
+			CDynaNodeBase *pNode = static_cast<CDynaNodeBase*>(*it);
+			if (pNode->IsStateOn() && (!pNode->IsLFTypePQ() || pNode->m_eLFNodeType == CDynaNodeBase::eLFNodeType::LFNT_BASE))
+			{
+				pNode->GetPnrQnr();
+				double Pe = pNode->GetSelfImbP() + pNode->Pg, Qe = pNode->GetSelfImbQ() + pNode->Qg;
+
+				CLinkPtrCount *pBranchLink = pNode->GetLink(0);
+				pNode->ResetVisited();
+				CDevice **ppDevice = nullptr;
+				while (pBranchLink->In(ppDevice))
+				{
+					CDynaBranch *pBranch = static_cast<CDynaBranch*>(*ppDevice);
+					if (pBranch->m_BranchState == CDynaBranch::BRANCH_ON)
+					{
+						CDynaNodeBase *pOppNode = pBranch->GetOppositeNode(pNode);
+						if (pOppNode->IsStateOn())
+						{
+							cplx mult = conj(pNode->VreVim);
+							cplx *pYkm = pBranch->m_pNodeIp == pNode ? &pBranch->Yip : &pBranch->Yiq;
+							mult *= pOppNode->VreVim ** pYkm;
+							Pe -= mult.real();
+							Qe += mult.imag();
+						}
+					}
+				}
+				pNode->Qg = Qe;
+				if (CDynaNodeBase::eLFNodeType::LFNT_BASE == pNode->m_eLFNodeType)
+					pNode->Pg = Pe;
+			}
+		}
+	}
+
+	for (DEVICEVECTORITR it = pNodes->begin(); it != pNodes->end(); it++)
+	{
+		CDynaNodeBase *pNode = static_cast<CDynaNodeBase*>(*it);
+		ATLTRACE("\n %20f %20f %20f %20f %20f %20f", pNode->V, pNode->Delta * 180 / M_PI, pNode->Pg, pNode->Qg, pNode->Pnr, pNode->Qnr);
+	}
 	return bRes;
 }
