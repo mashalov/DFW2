091880d2de47db52b04b91805c8ed370955c51d9
Eugene Mashalov <mashalov@gmail.com>
2019-06-07 15:45:26 +0500

    Optimized data structure for LF

--- DFW2/LoadFlow.h
+++ DFW2/LoadFlow.h
@@ -11,11 +11,23 @@ namespace DFW2
 	{
 	public:
 
+		// "виртуальная" ветвь для узла. Заменяет собой настоящую включенную ветвь или несколько
+		// включенных параллельных ветвей эквивалентным Y. Эти два параметра - все что нужно
+		// чтобы рассчитать небаланс узла и производные для смежных узлов
+		struct _VirtualBranch
+		{
+			cplx Y;
+			CDynaNodeBase *pNode;
+		};
+
 		// маппинг узла в строки матрица
 		struct _MatrixInfo
 		{
 			size_t nRowCount;														// количество элементов в строке матрицы
-			_MatrixInfo::_MatrixInfo() : nRowCount(0) {}
+			size_t nBranchCount;													// количество виртуальных ветвей от узла (включая БУ)
+			CDynaNodeBase *pNode;													// узел, к которому относится данное Info
+			_VirtualBranch *pBranches;												// список виртуальных ветвей узла
+			_MatrixInfo::_MatrixInfo() : nRowCount(0), nBranchCount(0) {}
 		};
 
 
@@ -32,6 +44,7 @@ namespace DFW2
 		CDynaNodeContainer *pNodes;
 		size_t m_nMatrixSize;
 		size_t m_nNonZeroCount;
+		size_t m_nBranchesCount;
 		
 
 		double *Ax;				// данные матрицы якоби
@@ -40,6 +53,9 @@ namespace DFW2
 		ptrdiff_t *Ap;			// номера столбцов
 
 		_MatrixInfo *m_pMatrixInfo;
+		_MatrixInfo *m_pMatrixInfoEnd;
+		_MatrixInfo *m_pMatrixInfoSlackEnd;
+		_VirtualBranch *m_pVirtualBranches;
 
 		KLU_symbolic *Symbolic;
 		KLU_common Common;
