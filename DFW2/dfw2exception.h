a731c142071489812a52327404246ecf77463e41
Евгений Машалов <mashalov@niipt-ems.ru>
2020-05-26 03:55:27 +0500

    Compiled to latest C++ (codecvt deprecation)

--- DFW2/dfw2exception.h
+++ DFW2/dfw2exception.h
@@ -1,19 +1,18 @@
-#pragma once
+﻿#pragma once
 #include <stdexcept>
-#include <string>
-#include <codecvt>
-#include <tchar.h>
 #include <memory>
+#include "utf8conv.h"
 
 class dfw2error : public std::runtime_error
 {
 private:
 	std::wstring m_Message;
-	using utf8conv = std::wstring_convert<std::codecvt_utf8<wchar_t>>;
 public:
-	dfw2error(std::wstring& Message) : runtime_error(std::make_unique<utf8conv>()->to_bytes(Message)), m_Message(Message) {}
+
+	dfw2error(std::wstring& Message) : runtime_error(utf8_encode(Message)), m_Message(Message) {}
 	dfw2error(dfw2error& err) : dfw2error(err.uwhat()) {}
-	dfw2error(const _TCHAR* Message) : runtime_error(std::make_unique<utf8conv>()->to_bytes(Message)), m_Message(Message) {}
+	dfw2error(const _TCHAR* Message) : runtime_error(utf8_encode(Message)), m_Message(Message) {}
+
 	const _TCHAR* uwhat()
 	{
 		return m_Message.c_str();
