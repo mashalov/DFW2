e653967b26e8b6ca3a25f068254f07ffcdcf930b
Евгений Машалов <mashalov@niipt-ems.ru>
2020-08-07 03:51:36 +0500

    TargetName change support

--- CustomDeviceCPP/build.bat
+++ CustomDeviceCPP/build.bat
@@ -1,2 +1,2 @@
-"C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\MSBuild\Current\Bin\msbuild.exe" /p:Platform=x64 /p:Configuration=Release /p:OutDir="c:\tmp\auto\\"  autodll.vcxproj /t:Rebuild 
+"C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\MSBuild\Current\Bin\msbuild.exe" /p:Platform=x64 /p:Configuration=Release /p:TargetName=pwm2000 /p:OutDir="c:\tmp\auto\\"  autodll.vcxproj /t:Rebuild 
 rem "C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\MSBuild\Current\Bin\msbuild.exe" /p:Platform=Win32 /p:Configuration=Release customdevicecpp.vcxproj
