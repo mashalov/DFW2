bb4bfb1b0e9ce0150b8fe94b12e5acf171f72b71
Евгений Машалов <mashalov@niipt-ems.ru>
2023-01-05 17:42:31 +0500

    Better parameters handling in DLL mode

--- DFW2/DynaGeneratorPark3C.cpp
+++ DFW2/DynaGeneratorPark3C.cpp
@@ -110,7 +110,7 @@ bool CDynaGeneratorPark3C::CalculateFundamentalParameters(PARK_PARAMETERS_DETERM
 	if (!bRes)
 		return bRes;
 
-	/*
+	///*
 	DebugLog(fmt::format("{} Rfd {:.3f} lfd {:.3f} R1d {:.3f} l1d {:.3f} R1q {:.3f} L1q {:.3f}",
 		GetVerbalName(),
 		Rfd, 
@@ -119,8 +119,8 @@ bool CDynaGeneratorPark3C::CalculateFundamentalParameters(PARK_PARAMETERS_DETERM
 		l1d, 
 		R1q, 
 		l1q));
-	*/
-	//
+	//*/
+	
 	CompareParksParameterCalculation();
 
 	const double lFd(lad + lfd);		// сопротивление обмотки возбуждения
@@ -169,7 +169,7 @@ bool CDynaGeneratorPark3C::CalculateFundamentalParameters(PARK_PARAMETERS_DETERM
 		Psiq_Psi1q = laq / l1Q;
 
 
-		/*
+		///*
 		DebugLog(fmt::format("{} lad {:.3f} lFd {:.3f} l1d {:.3f} l1Q {:.3f} Tdo1 {:.3f} Tdo2 {:.3f}",
 			GetVerbalName(),
 			lad,
@@ -179,7 +179,7 @@ bool CDynaGeneratorPark3C::CalculateFundamentalParameters(PARK_PARAMETERS_DETERM
 			lFd / Rfd,
 			l1D / R1d
 		));
-		*/
+		//*/
 	}
 
 	return bRes;
