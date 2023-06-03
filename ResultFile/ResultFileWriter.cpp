bf0eeaa05e4026b19af81ab1f44ff1748c55ea40
Евгений Машалов <mashalov@niipt-ems.ru>
2023-06-04 01:18:06 +0500

    Fixed result writer version

--- ResultFile/ResultFileWriter.cpp
+++ ResultFile/ResultFileWriter.cpp
@@ -22,18 +22,20 @@ void CResultFile::WriteString(std::string_view cszString)
 		WriteLEB(0);
 	else
 	{
-		
-#if DFW2_RESULTFILE_VERSION > 1
-		WriteLEB(cszString.size());
-		// for file versions > 1 write plain utf-8 strings
-		infile.write(cszString.data(), cszString.size());
-#else
-		// for older versions write scsu
-		std::wstring scsuToEncode = stringutils::utf8_decode(cszString);
-		WriteLEB(scsuToEncode.size());
-		CUnicodeSCSU StringWriter(infile);
-		StringWriter.WriteSCSU(scsuToEncode);
-#endif
+		if (Consts::dfw_result_file_version > 1)
+		{
+			WriteLEB(cszString.size());
+			// for file versions > 1 write plain utf-8 strings
+			infile.write(cszString.data(), cszString.size());
+		}
+		else
+		{
+			// for older versions write scsu
+			std::wstring scsuToEncode = stringutils::utf8_decode(cszString);
+			WriteLEB(scsuToEncode.size());
+			CUnicodeSCSU StringWriter(infile);
+			StringWriter.WriteSCSU(scsuToEncode);
+		}
 	}
 }
 
